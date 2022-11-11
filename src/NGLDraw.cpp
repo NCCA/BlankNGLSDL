52fbbc2f6ebabe16740d5f8ea6692bfc9513a408
jmacey <jmacey@bournemouth.ac.uk>
2022-11-11 11:34:37 +0000

    updated to include iostream as it was inadverently added in ngl

--- src/NGLDraw.cpp
+++ src/NGLDraw.cpp
@@ -2,100 +2,96 @@
 #include <ngl/ShaderLib.h>
 #include <ngl/NGLInit.h>
 #include <ngl/Transformation.h>
-const static float INCREMENT=0.01f;
-const static float ZOOM=0.05f;
+#include <iostream>
+const static float INCREMENT = 0.01f;
+const static float ZOOM = 0.05f;
 NGLDraw::NGLDraw()
 {
-  m_rotate=false;
+  m_rotate = false;
   // mouse rotation values set to 0
-  m_spinXFace=0;
-  m_spinYFace=0;
+  m_spinXFace = 0;
+  m_spinYFace = 0;
 
-  glClearColor(0.4f, 0.4f, 0.4f, 1.0f);			   // Grey Background
+  glClearColor(0.4f, 0.4f, 0.4f, 1.0f); // Grey Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
 }
 
 NGLDraw::~NGLDraw()
 {
-  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+  std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
 void NGLDraw::resize(int _w, int _h)
 {
-  glViewport(0,0,_w,_h);
+  glViewport(0, 0, _w, _h);
 }
 
 void NGLDraw::draw()
 {
   // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 }
 
-
-
 //----------------------------------------------------------------------------------------------------------------------
-void NGLDraw::mouseMoveEvent (const SDL_MouseMotionEvent &_event)
+void NGLDraw::mouseMoveEvent(const SDL_MouseMotionEvent &_event)
 {
-  if(m_rotate && _event.state &SDL_BUTTON_LMASK)
+  if (m_rotate && _event.state & SDL_BUTTON_LMASK)
   {
-    int diffx=_event.x-m_origX;
-    int diffy=_event.y-m_origY;
+    int diffx = _event.x - m_origX;
+    int diffy = _event.y - m_origY;
     m_spinXFace += 0.5f * diffy;
     m_spinYFace += 0.5f * diffx;
     m_origX = _event.x;
     m_origY = _event.y;
     draw();
-
   }
   // right mouse translate code
-  else if(m_translate && _event.state &SDL_BUTTON_RMASK)
+  else if (m_translate && _event.state & SDL_BUTTON_RMASK)
   {
     int diffX = static_cast<int>(_event.x - m_origXPos);
     int diffY = static_cast<int>(_event.y - m_origYPos);
-    m_origXPos=_event.x;
-    m_origYPos=_event.y;
+    m_origXPos = _event.x;
+    m_origYPos = _event.y;
     m_modelPos.m_x += INCREMENT * diffX;
     m_modelPos.m_y -= INCREMENT * diffY;
     draw();
   }
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------
-void NGLDraw::mousePressEvent (const SDL_MouseButtonEvent &_event)
+void NGLDraw::mousePressEvent(const SDL_MouseButtonEvent &_event)
 {
   // this method is called when the mouse button is pressed in this case we
   // store the value where the maouse was clicked (x,y) and set the Rotate flag to true
-  if(_event.button == SDL_BUTTON_LEFT)
+  if (_event.button == SDL_BUTTON_LEFT)
   {
     m_origX = _event.x;
     m_origY = _event.y;
-    m_rotate =true;
+    m_rotate = true;
   }
   // right mouse translate mode
-  else if(_event.button == SDL_BUTTON_RIGHT)
+  else if (_event.button == SDL_BUTTON_RIGHT)
   {
     m_origXPos = _event.x;
     m_origYPos = _event.y;
-    m_translate=true;
+    m_translate = true;
   }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLDraw::mouseReleaseEvent (const SDL_MouseButtonEvent &_event)
+void NGLDraw::mouseReleaseEvent(const SDL_MouseButtonEvent &_event)
 {
   // this event is called when the mouse button is released
   // we then set Rotate to false
   if (_event.button == SDL_BUTTON_LEFT)
   {
-    m_rotate=false;
+    m_rotate = false;
   }
   // right mouse translate mode
   if (_event.button == SDL_BUTTON_RIGHT)
   {
-    m_translate=false;
+    m_translate = false;
   }
 }
 
@@ -104,26 +100,26 @@ void NGLDraw::wheelEvent(const SDL_MouseWheelEvent &_event)
 {
 
   // check the diff of the wheel position (0 means no change)
-  if(_event.y > 0)
+  if (_event.y > 0)
   {
-    m_modelPos.m_z+=ZOOM;
+    m_modelPos.m_z += ZOOM;
     this->draw();
   }
-  else if(_event.y <0 )
+  else if (_event.y < 0)
   {
-    m_modelPos.m_z-=ZOOM;
+    m_modelPos.m_z -= ZOOM;
     draw();
   }
 
   // check the diff of the wheel position (0 means no change)
-  if(_event.x > 0)
+  if (_event.x > 0)
   {
-    m_modelPos.m_x-=ZOOM;
+    m_modelPos.m_x -= ZOOM;
     draw();
   }
-  else if(_event.x <0 )
+  else if (_event.x < 0)
   {
-    m_modelPos.m_x+=ZOOM;
+    m_modelPos.m_x += ZOOM;
     draw();
   }
 }
